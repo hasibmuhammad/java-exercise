f1d2bc223e77f5316aa1f7556d4217bc35002fbb
hasibmuhammad <hasibmu99@gmail.com>
2022-01-05 15:05:53 +0600

    Exercise 9 - Done

---
+++ Ex8.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Exercises;
+
+/**
+ *
+ * @author Storyteller
+ */
+public class Ex8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+    
+}
---
+++ Ex9.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Exercises;
+
+/**
+ *
+ * @author Storyteller
+ */
+public class Ex9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double expr = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+        System.out.print(expr);
+    }
+    
+}
